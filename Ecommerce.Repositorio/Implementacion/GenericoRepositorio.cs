5270399ef3c02a5b2e44542f9fe0a78d02bce81c
Amimbrer <136268910+Amimbrer@users.noreply.github.com>
2023-07-21 12:03:32 +0200

    15 primeros minutos video 2

---
+++ Ecommerce.Repositorio/Implementacion/GenericoRepositorio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+// referencias necesarias
+using Ecommerce.Repositorio.Contrato;
+using Ecommerce.Repositorio.DBContext;
+
+namespace Ecommerce.Repositorio.Implementacion {
+    public class GenericoRepositorio<TModelo> : IGenericoRepositorio<TModelo> where TModelo : class {
+        private readonly DbecommerceContext _dbContext;
+
+        public GenericoRepositorio(DbecommerceContext dbContext) {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<TModelo> Consultar(Expression<Func<TModelo, bool>>? filtro = null) {
+            throw new NotImplementedException();
+        }
+
+        public Task<TModelo> Crear(TModelo modelo) {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Editar(TModelo modelo) {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(TModelo modelo) {
+            throw new NotImplementedException();
+        }
+    }
+}
